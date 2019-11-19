72b64156d92c2d1de23d70cffe50374d342690db
Chris Harvey <chris@chrisnharvey.com>
2019-11-19 00:14:56 +0000

    Save contents of home directory to a data directory

--- containers/workspace/magicLAMP.bash
+++ containers/workspace/magicLAMP.bash
@@ -3,6 +3,11 @@ export SHELL=/bin/bash
 cat /etc/magicLAMP.art
 
 source /magicLAMP/.env
+
+sudo chown magicLAMP:magicLAMP /home/magicLAMP
+sudo touch /home/magicLAMP/.bash_history
+sudo chown magicLAMP:magicLAMP /home/magicLAMP/.bash_history
+
 sudo update-alternatives --set php "/usr/bin/php${DEFAULT_PHP_VERSION}" &> /dev/null
 sudo update-alternatives --set phar "/usr/bin/phar${DEFAULT_PHP_VERSION}" &> /dev/null
 sudo update-alternatives --set phar.phar "/usr/bin/phar.phar${DEFAULT_PHP_VERSION}" &> /dev/null
