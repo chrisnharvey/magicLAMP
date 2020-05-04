6d5a02384384bd36b339f338ecfc43ce6b4eb6cd
Chris Harvey <chris@chrisnharvey.com>
2020-05-04 23:27:04 +0100

    Add default magicLAMP home directory

--- containers/default/workspace/magicLAMP.sh
+++ containers/default/workspace/magicLAMP.sh
@@ -8,9 +8,5 @@ NEW_VERSION=$DEFAULT_PHP_VERSION
 SESSION_SWITCH=1
 source /usr/src/magicLAMP/switch-php-version
 
-sudo chown magicLAMP:magicLAMP /home/magicLAMP
-
-cp -n /opt/magicLAMP/zshrc /home/magicLAMP/.zshrc
-
 git config --global user.name "${GIT_NAME}"
 git config --global user.email "${GIT_EMAIL}"
