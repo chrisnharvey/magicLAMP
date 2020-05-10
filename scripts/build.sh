3b81c1a29a117e3bf05057b9e41bc92c237ee42b
Chris Harvey <chris@chrisnharvey.com>
2020-05-10 22:26:33 +0100

    Update build script to build a single container from input argument

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,17 +4,14 @@
 
 translateDockerTag
 
-for container_path in ${BASH_SOURCE%/*}/../containers/default/* ; do
+container_path="$1"
+container_name="chrisnharvey/magiclamp-$(basename $container_path)"
 
-    container_name="chrisnharvey/magiclamp-$(basename $container_path)"
+BUILD_TAGS=""
 
-    BUILD_TAGS=""
-
-    for TAG in ${TAGS}
-    do
-      BUILD_TAGS="${BUILD_TAGS}-t ${container_name}:${TAG} "
-    done
-
-    docker build ${BUILD_TAGS} ${container_path}
-
+for TAG in ${TAGS}
+do
+    BUILD_TAGS="${BUILD_TAGS}-t ${container_name}:${TAG} "
 done
+
+docker build ${BUILD_TAGS} ${container_path}
