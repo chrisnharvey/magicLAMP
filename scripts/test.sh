4e5edaf9ec682f1b40cbb6baf677a4c260b15a7c
Chris Harvey <chris@chrisnharvey.com>
2020-05-11 18:50:55 +0100

    Add MAGICLAMP_VERSION environment variable and update tests to override this

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+. "${BASH_SOURCE%/*}/utils.sh"
+
+translateDockerTag
+
+MAGICLAMP_VERSION=$(echo $TAGS | cut -d ' ' -f 1)
+
 EXIT_CODE=0
 
+cp ${BASH_SOURCE%/*}/../.env.example ${BASH_SOURCE%/*}/../.env
+
 for test_path in ${BASH_SOURCE%/*}/../tests/* ; do
-    GOSS_FILES_PATH="${test_path}" dcgoss run "$(basename $test_path)"
+    MAGICLAMP_VERSION=$MAGICLAMP_VERSION GOSS_FILES_PATH="${test_path}" dcgoss run "$(basename $test_path)"
 
     if [ "$?" -ne 0 ] ; then
         EXIT_CODE=1
