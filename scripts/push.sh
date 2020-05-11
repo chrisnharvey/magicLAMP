b1fcf2b9c90a5e11ebf5626d572af4ed96fc8291
Chris Harvey <chris@chrisnharvey.com>
2020-05-11 18:51:03 +0100

    Get container name before pushing

--- scripts/push.sh
+++ scripts/push.sh
@@ -6,6 +6,8 @@ translateDockerTag
 
 for container_path in ${BASH_SOURCE%/*}/../containers/default/* ; do
 
+    container_name="chrisnharvey/magiclamp-$(basename $container_path)"
+
     for TAG in ${TAGS}
     do
         docker push "${container_name}:${TAG}"
