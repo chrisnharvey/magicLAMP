1e7cf17b2ed3ea7a4623ae263dcb01f05fe3f251
Chris Harvey <chris@chrisnharvey.com>
2020-08-07 19:58:37 +0100

    Add the ability for the workspace to revive itself after restarting

--- shell.sh
+++ shell.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+DIR=`dirname $0`
+
 docker-compose exec -u magicLAMP workspace /bin/zsh
+
+REVIVE="$DIR/.revive"
+
+if [ -f "$REVIVE" ]; then
+    rm -f "$REVIVE"
+    docker-compose up -d
+    ./shell.sh
+fi
