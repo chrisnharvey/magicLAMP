63242dd9ac9d94d5e8cfe1d8a4ec4e360b350fa8
Chris Harvey <chris@chrisnharvey.com>
2020-08-12 00:23:51 +0100

    Pass magiclamp host directory to workspace when running shell

--- shell.sh
+++ shell.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-DIR=`dirname $0`
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker-compose exec -u magicLAMP workspace /bin/zsh
+docker-compose exec -e MAGICLAMP_HOST_DIR=$DIR -u magicLAMP workspace /bin/zsh
 
 REVIVE="$DIR/.revive"
 
